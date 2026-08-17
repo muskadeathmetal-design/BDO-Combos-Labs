eae9a178f45e5ed626ddab8159daf68afd840f2e
muskadeathmetal-design <muskadeathmetal@gmail.com>
2026-08-17 16:03:45 +0200

    Fix whole-word translation regex in safe V116 build

--- tools/run-safe-static-build-v116.js
+++ tools/run-safe-static-build-v116.js
@@ -10,14 +10,16 @@ if (!src.includes(needle)) {
   throw new Error('Expected translation replacement line not found');
 }
 
-const replacement = String.raw`    const singleToken = /^[A-Za-zÀ-ÖØ-öø-ÿŒœÆæ'-]+$/u.test(from);
-    if (singleToken) {
-      const escaped = from.replace(/[.*+?^\${}()|[\]\\]/g, '\\$&');
-      const re = new RegExp('(^|[^A-Za-zÀ-ÖØ-öø-ÿŒœÆæ\\\'-])(' + escaped + ')(?=$|[^A-Za-zÀ-ÖØ-öø-ÿŒœÆæ\\\'-])', 'gu');
-      out = out.replace(re, function(match, lead){ return lead + to; });
-    } else {
-      out = out.split(from).join(to);
-    }`;
+const replacement = [
+  "    const singleToken = /^[\\p{L}\\p{M}'-]+$/u.test(from);",
+  "    if (singleToken) {",
+  "      const escaped = from.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&');",
+  "      const re = new RegExp('(^|[^\\\\p{L}\\\\p{M}\\\'-])(' + escaped + ')(?=$|[^\\\\p{L}\\\\p{M}\\\'-])', 'gu');",
+  "      out = out.replace(re, function(match, lead){ return lead + to; });",
+  "    } else {",
+  "      out = out.split(from).join(to);",
+  "    }"
+].join('\n');
 
 src = src.replace(needle, replacement);
 fs.writeFileSync(tmpFile, src, 'utf8');
