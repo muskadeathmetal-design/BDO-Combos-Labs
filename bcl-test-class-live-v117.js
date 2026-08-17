96573ae18338d4ddd0305c78c16d9ebe4462abbf
muskadeathmetal-design <muskadeathmetal@gmail.com>
2026-08-18 01:13:52 +0200

    Load AnalysisCore CC state bridge v131

--- bcl-test-class-live-v117.js
+++ bcl-test-class-live-v117.js
@@ -5,5 +5,5 @@ function load(src,id){
 }
 load('/bcl-test-native-v128.js?v=128','bcl-test-native-v128');
 load('/bcl-analysis-settings-v121.js?v=127','bcl-analysis-settings-v127');
-load('/bcl-analysiscore-builder-v129.js?v=130','bcl-analysiscore-builder-v130');
+load('/bcl-analysiscore-builder-v129.js?v=131','bcl-analysiscore-builder-v131');
 })();
