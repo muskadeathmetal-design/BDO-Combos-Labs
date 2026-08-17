a92310391383e3a1706992ec8e420d8c99f867ca
muskadeathmetal-design <muskadeathmetal@gmail.com>
2026-08-17 15:58:40 +0200

    Add safe whole-word static build wrapper

---
+++ tools/run-safe-static-build-v116.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const cp = require('child_process');
+
+const srcFile = 'tools/build-static-locales-v116.js';
+const tmpFile = 'tools/.build-static-locales-v116-safe.tmp.js';
+let src = fs.readFileSync(srcFile, 'utf8');
+
+const needle = "    out = out.split(from).join(to);";
+if (!src.includes(needle)) {
+  throw new Error('Expected translation replacement line not found');
+}
+
+const replacement = String.raw`    const singleToken = /^[A-Za-zÀ-ÖØ-öø-ÿŒœÆæ'-]+$/u.test(from);
+    if (singleToken) {
+      const escaped = from.replace(/[.*+?^\${}()|[\]\\]/g, '\\$&');
+      const re = new RegExp('(^|[^A-Za-zÀ-ÖØ-öø-ÿŒœÆæ\\\'-])(' + escaped + ')(?=$|[^A-Za-zÀ-ÖØ-öø-ÿŒœÆæ\\\'-])', 'gu');
+      out = out.replace(re, function(match, lead){ return lead + to; });
+    } else {
+      out = out.split(from).join(to);
+    }`;
+
+src = src.replace(needle, replacement);
+fs.writeFileSync(tmpFile, src, 'utf8');
+
+try {
+  cp.execFileSync(process.execPath, ['--check', tmpFile], { stdio: 'inherit' });
+  cp.execFileSync(process.execPath, [tmpFile], { stdio: 'inherit' });
+} finally {
+  try { fs.unlinkSync(tmpFile); } catch (_) {}
+}
