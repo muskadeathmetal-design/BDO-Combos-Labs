0e654a4afe5f3c84afc4620ba5515342e48043d3
muskadeathmetal-design <muskadeathmetal@gmail.com>
2026-08-18 02:20:55 +0200

    Force load compact action toolbar V150

--- bcl-test-class-live-v117.js
+++ bcl-test-class-live-v117.js
@@ -9,5 +9,5 @@ load('/bcl-analysiscore-builder-v129.js?v=140','bcl-analysiscore-builder-v140');
 load('/bcl-combat-profile-safe-v141.js?v=141','bcl-combat-profile-safe-v141');
 load('/bcl-phase-target-safe-v142.js?v=142','bcl-phase-target-safe-v142');
 load('/bcl-combat-coach-safe-v143.js?v=143','bcl-combat-coach-safe-v143');
-load('/bcl-builder-harmony-v144.js?v=149','bcl-builder-workspace-v149');
+load('/bcl-builder-harmony-v144.js?v=150','bcl-builder-workspace-v150');
 })();
